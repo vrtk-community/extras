f366a704cb5a412f76b8b79a64d123486538f874
Dustin Lacewell <dlacewell@gmail.com>
2019-12-08 00:34:50 -0600

    Add asmdef and more prefabs and scripts

---
+++ Runtime/Prefabs/Interactables/Grab Setup/Scripts/GrabActionSetup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using VRTK.Prefabs.Interactions.Interactables.Grab.Action;
+using Zenject;
+
+public class GrabActionSetup : MonoBehaviour
+{
+    [Serializable]
+    public class UnityEvent : UnityEvent<GrabInteractableAction> { }
+    
+    public GameObject ActionContainer;
+    public UnityEvent SetupEvent;
+
+    [Inject]
+    public void Construct()
+    {
+        if (ActionContainer == null) return;
+
+        var action = ActionContainer.GetComponentInChildren<GrabInteractableAction>();
+
+        if (action == null) return;
+        
+        SetupEvent?.Invoke(action);
+    }
+}
